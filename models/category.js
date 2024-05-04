bbaf23bd48c58064052597c34f1c50bc92f7caf2
kruti2511 <47136397+kruti2511@users.noreply.github.com>
2024-05-04 08:39:23 +0530

    Create category.js

---
+++ models/category.js
@@ -0,0 +1,14 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Category = sequelize.define('Category', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING
+  }
+});
+
+module.exports = Category;
