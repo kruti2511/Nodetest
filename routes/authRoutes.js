47e5281eb6c43979c8b21fa4f5c7fb73800970ee
kruti2511 <47136397+kruti2511@users.noreply.github.com>
2024-05-04 08:53:58 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const { body } = require('express-validator');
 
-router.post('/login', login);
+router.post('/login', [
+  body('username').isEmail(),
+  body('password').isLength({ min: 5 })
+], login);
 
 module.exports = router;
